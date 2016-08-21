ccd6174d8a42690873f39c3216f05ed410908399
rohan-bhagat <me@rohanbhagat.com>
2016-08-21 18:19:15 +0530

    Fixed typo in comment

--- tracks/files/default/bundle_config.sh
+++ tracks/files/default/bundle_config.sh
@@ -4,7 +4,7 @@ mv /tmp/database.yml /opt/tracks/current/config/database.yml
 mv /tmp/site.yml /opt/tracks/current/config/site.yml
 #workaround to fix symlink issues during bundle exec 
 mkdir /opt/{log,pids}
-#change wor dir
+#change work dir
 cd /opt/tracks/current
 bundle exec rake db:migrate RAILS_ENV=production
 bundle exec rake assets:precompile RAILS_ENV=production
